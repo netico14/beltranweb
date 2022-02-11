3a635ad74dcfb010f17d3e525e27e9ab1d4a71aa
Ernesto Beltran <e.beltran@akimad.com>
2022-02-10 21:26:41 -0500

    add meta security

--- next.config.js
+++ next.config.js
@@ -1,3 +1,59 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' nonce;
+  child-src nonce;
+  style-src 'self' nonce;
+  font-src 'self';  
+`
+
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+  // {
+  //   key: 'Content-Security-Policy',
+  //   value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  // }
+]
+
 module.exports = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["es"],
+    defaultLocale: "es",
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  }
 }
